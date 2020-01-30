716f24db073a929e07ed492565bd7531470a86fc
MarioBlanco <mbalonso2@gmail.com>
2020-01-30 11:50:37 +0100

    first

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          TESTING THIS NEW EXCITING CICD PIPELINE OMG
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
